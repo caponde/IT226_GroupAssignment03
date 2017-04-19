2a9b7be3e6c37a41b443d359e6e985c19acdf395
Cody Ponder <Cody Ponder>
2017-04-19 11:33:53 -0500

    Added add button to new activity

---
+++ app/src/main/java/isu/it226/it226_groupassignment03/CreateAlarmActivity.java
@@ -0,0 +1,15 @@
+package isu.it226.it226_groupassignment03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAlarmActivity extends AppCompatActivity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_alarm);
+	}
+}
--- app/src/main/java/isu/it226/it226_groupassignment03/MainActivity.java
+++ app/src/main/java/isu/it226/it226_groupassignment03/MainActivity.java
@@ -1,15 +1,22 @@
 package isu.it226.it226_groupassignment03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	public void createAlarm(View view)
+	{
+		Intent intent = new Intent(this, CreateAlarmActivity.class);
+		startActivity(intent);
 	}
 }
