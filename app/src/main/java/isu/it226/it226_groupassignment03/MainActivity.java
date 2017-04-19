e7435a59eec7e557e36146b752d5cd7df3b16017
Cody Ponder <Cody Ponder>
2017-04-19 11:14:15 -0500

    Simple UI layout

---
+++ app/src/main/java/isu/it226/it226_groupassignment03/MainActivity.java
@@ -0,0 +1,15 @@
+package isu.it226.it226_groupassignment03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
